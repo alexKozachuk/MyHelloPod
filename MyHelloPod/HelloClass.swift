731e2195353a9ba1b576aa8b61a93d51683e2456
alexKozachuk <grayid@icloud.com>
2021-01-22 17:12:44 +0200

    Initial Commit

---
+++ MyHelloPod/HelloClass.swift
@@ -0,0 +1,14 @@
+//
+//  HelloClass.swift
+//  MyHelloPod
+//
+//  Created by Sasha on 22/01/2021.
+//
+
+import Foundation
+
+class HelloClass {
+    func hello() {
+        print("Hello world")
+    }
+}
---
+++ MyHelloPod/MyHelloPod.h
@@ -0,0 +1,18 @@
+//
+//  MyHelloPod.h
+//  MyHelloPod
+//
+//  Created by Sasha on 22/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyHelloPod.
+FOUNDATION_EXPORT double MyHelloPodVersionNumber;
+
+//! Project version string for MyHelloPod.
+FOUNDATION_EXPORT const unsigned char MyHelloPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyHelloPod/PublicHeader.h>
+
+
